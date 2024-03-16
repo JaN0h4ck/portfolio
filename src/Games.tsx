e621f036a2e400d686ac0339007c7c69d9ae9194
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-03-16 19:20:46 +0100

    Switch Row and Col

--- src/Games.tsx
+++ src/Games.tsx
@@ -1,7 +1,5 @@
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
-import Row from "react-bootstrap/Row";
 import { ListItem } from "./partials/ListItem";
 
 export function Games() {
@@ -10,20 +8,16 @@ export function Games() {
 			<h1 className="text-center">Games</h1>
 			<Container>
 				{/* className="d-flex flex-column justify-content-center align-items-center"> */}
-				<Row>
-					<Col>
-						<h3>WickedTD</h3>
-						<Image src="/img/wickedtd_cropped.png" fluid></Image>
-						<ul>
-							<li>Child 1</li>
-							<li>Child 2</li>
-							<li>Child 3</li>
-						</ul>
-					</Col>
+				<Col>
+					<ListItem
+						title="WickedTD"
+						imgSrc="/img/wickedtd_cropped.png"
+						bulletPoints={["Game Jam Project during Brackey Game Jam 2023.1", "My Role: Programmer, "]}
+					/>
 					<ListItem title="Republic Bread" bulletPoints={["Project", "Project Lead", "etc."]} imgSrc="" />
 					<div>item 2</div>
 					<div>item 3</div>
-				</Row>
+				</Col>
 			</Container>
 		</>
 	);
