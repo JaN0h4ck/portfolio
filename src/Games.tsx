84209875bca94fbb4fd4ad6d1fb025f2715b6bae
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-03-16 18:31:15 +0100

    Create Games Page

---
+++ src/Games.tsx
@@ -0,0 +1,16 @@
+export function Games() {
+	//Page Title
+	//List
+	//Picture
+	//short description
+	return (
+		<>
+			<h1 className="text-center">Games</h1>
+			<div className="d-flex flex-column justify-content-center align-items-center">
+				<div>item 1</div>
+				<div>item 2</div>
+				<div>item 3</div>
+			</div>
+		</>
+	);
+}
