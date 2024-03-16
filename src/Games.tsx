60056a6072f397c751fd0acec9f9c2e1eb5bb324
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-03-16 19:03:31 +0100

    Trial Layout for List

--- src/Games.tsx
+++ src/Games.tsx
@@ -1,16 +1,28 @@
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Row from "react-bootstrap/Row";
+
 export function Games() {
-	//Page Title
-	//List
-	//Picture
-	//short description
 	return (
 		<>
 			<h1 className="text-center">Games</h1>
-			<div className="d-flex flex-column justify-content-center align-items-center">
-				<div>item 1</div>
-				<div>item 2</div>
-				<div>item 3</div>
-			</div>
+			<Container>
+				{/* className="d-flex flex-column justify-content-center align-items-center"> */}
+				<Row>
+					<Col>
+						<h3>WickedTD</h3>
+						<Image src="/img/wickedtd_cropped.png" fluid></Image>
+						<ul>
+							<li>Child 1</li>
+							<li>Child 2</li>
+							<li>Child 3</li>
+						</ul>
+					</Col>
+					<div>item 2</div>
+					<div>item 3</div>
+				</Row>
+			</Container>
 		</>
 	);
 }
