781e746fa48e291393af2ebfa8b538fc93795df9
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2024-03-20 13:22:45 +0100

    create Theme Mode Provider

---
+++ src/providers/ThemeModeProvider.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+type Theme = "light" | "dark" | "system";
+
+type ThemeContextProps = {
+	theme: Theme;
+	setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+};
+
+type Props = {
+	children: React.ReactNode;
+};
+
+const initialProps: ThemeContextProps = {
+	theme: "light",
+	setTheme: () => {},
+};
+
+const ThemeContext = createContext<ThemeContextProps>(initialProps);
+
+function ThemeModeProvider({ children }: Props) {
+	const [theme, setTheme] = useState<Theme>("light");
+
+	useEffect(() => {
+		if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+			setTheme("dark");
+		}
+	}, []);
+
+	return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
+}
+
+const useTheme = () => useContext(ThemeContext);
+
+export { ThemeModeProvider, useTheme };
