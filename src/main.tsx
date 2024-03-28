aee484a9b9eee44fcb98276317962eb727c1fa58
Jan Nothacker <git@jan-nothacker.de>
2024-03-28 21:23:30 +0100

    customize bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemeModeProvider } from "./providers/ThemeModeProvider.tsx";
 import { I18nProvider } from "./providers/I18nProvider.tsx";
 
