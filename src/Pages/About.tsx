3a7c311516083719fbbc493501f570892b9eae58
Jan Nothacker <git@jan-nothacker.de>
2024-03-29 11:27:38 +0100

    Add About Page

---
+++ src/Pages/About.tsx
@@ -0,0 +1,10 @@
+import Container from "react-bootstrap/Container";
+
+export function About() {
+	return (
+		<>
+			<h1 className="text-center fw-bold mb-4">About me</h1>
+			<Container></Container>
+		</>
+	);
+}
