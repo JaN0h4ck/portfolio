19747afdf30bf5d82f96131a783019ad62be7ed3
Jan Nothacker <git@jan-nothacker.de>
2024-03-29 11:48:56 +0100

    proper static asset handling

--- src/Pages/Games.tsx
+++ src/Pages/Games.tsx
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 import { useI18n } from "../providers/I18nProvider";
 import * as en from "../texts/games/en.json";
 import * as de from "../texts/games/de.json";
+import robotsImgUrl from "/src/assets/img/robots_env.png";
+import wickedtdImgUrl from "/src/assets/img/wickedtd_cropped.png";
+import breadImgUrl from "/src/assets/img/Villa_Final.jpg";
 
 export function Games() {
 	const { lang } = useI18n();
@@ -16,7 +19,7 @@ export function Games() {
 				<Col>
 					<ListItem
 						title="Robots vs. Wrestlers (without Wrestlers)"
-						imgSrc="/img/robots_env.png"
+						imgSrc={robotsImgUrl}
 						bulletPoints={lang == "en" ? en.robots : de.robots}
 						lastChild={
 							<a className="btn btn-primary mt-3" href="https://jan0h4ck.itch.io/robotsvswrestlers" target="blank">
@@ -28,7 +31,7 @@ export function Games() {
 				<Col>
 					<ListItem
 						title="WickedTD"
-						imgSrc="/img/wickedtd_cropped.png"
+						imgSrc={wickedtdImgUrl}
 						bulletPoints={lang == "en" ? en.wickedtd : de.wickedtd}
 						lastChild={
 							<a className="btn btn-primary mt-3" href="https://jan0h4ck.itch.io/wickedtd" target="blank">
@@ -39,7 +42,7 @@ export function Games() {
 					<ListItem
 						title="Republic Bread"
 						bulletPoints={lang == "en" ? en.bread : de.bread}
-						imgSrc="/img/Villa_Final.jpg"
+						imgSrc={breadImgUrl}
 						lastChild={
 							<Link className="btn btn-primary mt-3" to="/rb">
 								Learn More
