73b96aa8ac9b190d2571e0f24a06a031a58711b3
Jan Nothacker <git@jan-nothacker.de>
2024-03-29 19:24:26 +0100

    fix sizing issues on mobile

--- src/partials/ListItem.tsx
+++ src/partials/ListItem.tsx
@@ -14,7 +14,7 @@ export function ListItem({ imgSrc, title, bulletPoints, lastChild, children }: P
 		<Row className="mb-5">
 			<h3 className="fw-bold mb-3">{title}</h3>
 			{imgSrc === "" ? "" : <Image src={imgSrc} className="mb-3" fluid></Image>}
-			<ul>
+			<ul className="mx-3">
 				{bulletPoints.map((point, index) => {
 					return <li key={`${title}-${index}`}>{point}</li>;
 				})}
