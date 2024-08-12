b2817d75c6c47b73287df7bf9389b2adfaf65d5d
JaN0h4ck <git@jan-nothacker.de>
2024-08-12 19:35:56 +0200

    containerize everything

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -11,12 +11,14 @@ export default function About() {
 		<>
 			<h1 className="text-center fw-bold mb-4">{lang == "en" ? en.title : de.title}</h1>
 			<Container>
-				<div className="d-flex flex-column align-items-center">
-					<Image src={benniImgUrl} roundedCircle width={"250svw"}></Image>
-					<h3 className="fw-bold my-3">Jan Nothacker</h3>
-					<p>{lang == "en" ? en.subtitle : de.subtitle}</p>
-					<p>{lang == "en" ? en.paragraph : de.paragraph}</p>
-					<a className="btn btn-primary" target="blank" href="https://www.linkedin.com/in/jan-robert-nothacker-0a7b43264/">{lang == "en" ? en.btn : de.btn}</a>
+				<div className="custom-container mb-5 pt-3 pb-4">
+					<div className="d-flex flex-column align-items-center">
+						<Image src={benniImgUrl} roundedCircle width={"250svw"}></Image>
+						<h3 className="fw-bold my-3">Jan Nothacker</h3>
+						<p>{lang == "en" ? en.subtitle : de.subtitle}</p>
+						<p>{lang == "en" ? en.paragraph : de.paragraph}</p>
+						<a className="btn btn-primary" target="blank" href="https://www.linkedin.com/in/jan-robert-nothacker-0a7b43264/">{lang == "en" ? en.btn : de.btn}</a>
+					</div>
 				</div>
 			</Container>
 		</>
