9276ada3dd2751f7e24baea00ae643d2a5c2dc50
Jan Nothacker <git@jan-nothacker.de>
2025-03-04 08:46:12 +0100

    FIX i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	site: "https://portfolio.jan-nothacker.de",
+	i18n: {
+		locales: ["en", "de"],
+		defaultLocale: "en",
+		routing: {
+			prefixDefaultLocale: true,
+		},
+	},
 });
